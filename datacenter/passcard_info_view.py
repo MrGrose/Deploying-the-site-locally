dcd49dfa3c2f94d1031d9d162dcc0a8b42a347f7
MrGrose <grachev.ro@gmail.com>
2024-12-24 13:33:11 +0300

    #

---
+++ datacenter/passcard_info_view.py
@@ -0,0 +1,29 @@
+from datacenter.models import Passcard, Visit
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from datacenter.retrieve_and_format_duration import format_duration
+from datacenter.has_long_visit import is_visit_long
+
+
+def passcard_info_view(request, passcode):
+
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    filter_visits = Visit.objects.filter(passcard=passcard)
+    utc_now = timezone.now()
+    this_passcard_visits = []
+    for visit in filter_visits:
+        leaved_at = visit.leaved_at if visit.leaved_at else utc_now
+        delta_enter = leaved_at - visit.entered_at
+        total_seconds = int(delta_enter.total_seconds())
+        long_visit = is_visit_long(total_seconds)
+        formatted_duration = format_duration(delta_enter)
+        this_passcard_visits.append({
+            'entered_at': visit.entered_at.strftime("%d-%m-%Y %H:%M"),
+            'duration': formatted_duration,
+            'is_strange': long_visit,
+        })
+    context = {
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
+    }
+    return render(request, 'passcard_info.html', context)
---
+++ project/settings.py
@@ -0,0 +1,48 @@
+import os
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': env.str('HOST'),
+        'PORT': env.str('PORT'),
+        'NAME': env.str('NAME'),
+        'USER': env.str('USER'),
+        'PASSWORD': env.str('PASSWORD'),
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = env.str('SECRET_KEY')
+
+DEBUG = env.bool('DEBUG')
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = ['*']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = env.str('LANGUAGE_CODE')
+
+TIME_ZONE = env.str('TIME_ZONE')
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
