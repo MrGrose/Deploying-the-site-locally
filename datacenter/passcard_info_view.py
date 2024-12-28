480fd9cdcf81cede8975b967ccc1536060ed7273
MrGrose <grachev.ro@gmail.com>
2024-12-28 11:15:35 +0300

    Fixed code
    - Delegated the calculation to get_visit_duration
    - Used get_duration for a long visit
    - The logic of calculating the "strangeness" of the visit should be delegated to is_visits_long, avoiding an excessive call to total_seconds.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,29 +1,35 @@
 from datacenter.models import Passcard, Visit
-from datacenter.retrieve_and_format_duration import (format_duration,
-                                                     is_visit_long)
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from datacenter.retrieve_and_format_duration import format_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
-
     passcard = get_object_or_404(Passcard, passcode=passcode)
     filter_visits = Visit.objects.filter(passcard=passcard)
-    utc_now = timezone.now()
-    this_passcard_visits = []
-    for visit in filter_visits:
-        leaved_at = visit.leaved_at if visit.leaved_at else utc_now
-        delta_enter = leaved_at - visit.entered_at
-        total_seconds = int(delta_enter.total_seconds())
-        long_visit = is_visit_long(total_seconds)
-        formatted_duration = format_duration(delta_enter)
-        this_passcard_visits.append({
-            'entered_at': visit.entered_at.strftime("%d-%m-%Y %H:%M"),
-            'duration': formatted_duration,
-            'is_strange': long_visit,
-        })
+    
+    this_passcard_visits = [process_visit(visit) for visit in filter_visits]
+    
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
     }
+    
     return render(request, 'passcard_info.html', context)
+
+
+def get_visit_duration(visit):
+    utc_now = timezone.now()
+    leaved_at = visit.leaved_at if visit.leaved_at else utc_now
+    delta_enter = leaved_at - visit.entered_at
+    return delta_enter
+
+
+def process_visit(visit):
+    duration = get_visit_duration(visit)
+    total_seconds = int(duration.total_seconds())
+    return {
+        'entered_at': visit.entered_at.strftime("%d-%m-%Y %H:%M"),
+        'duration': format_duration(duration),
+        'is_strange': is_visit_long(total_seconds),
+    }
